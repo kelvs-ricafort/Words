73ea96f11d53abfbabeb60702006941197285389
Kelvin Ricafort <kelvs.r@outlook.com>
2023-12-01 17:00:18 +0800

    Test Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
+    androidTestImplementation("androidx.navigation:navigation-testing:2.7.5")
+
+
+    debugImplementation("androidx.fragment:fragment-testing:1.6.2")
 }
